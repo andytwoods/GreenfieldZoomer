4ff3086fe4066026d7f15ce29d0157fd15d81dd6
andy <andytwoods@gmail.com>
2020-04-24 08:25:16 +0100

    all done via command line gui for time being. Does the job.

--- zoomer.py
+++ zoomer.py
@@ -1,9 +1,12 @@
+import csv
+
 import pyautogui
 import time
 
-
 # majority of code stolen from
 # https://github.com/BigchillRK/Zoom-Meeting-and-Recording/blob/master/zoom_record_template1.py
+from pymsgbox import prompt
+
 
 def join_zoom(meet_id: str, password: str = None):
     pyautogui.press('esc', interval=0.1)
@@ -39,4 +42,61 @@ def join_zoom(meet_id: str, password: str = None):
     input()
 
 
-join_zoom('123456', '796')
+def load_details():
+    creds = []
+
+    try:
+        details = csv.reader(open('details.csv'))
+        for detail in details:
+            if len(detail) > 0:  # csv saving with a blank line between creds for some reason. oh well.
+                creds.append(detail)
+    except FileNotFoundError:
+        data = prompt(
+            text='Please copy paste below the contents from '
+                 '"Zoom Meeting Join Details.pdf" sent in an email on 17th April by Mr Brown.',
+            title='No teacher Zoom credentials on record')
+
+        for line in data.split('\n'):
+            parts = line.split(' ')
+            if len(parts) < 3:
+                continue
+            teacher = ' '.join(parts[0:2])
+            password = parts.pop()
+            meeting_id = ' '.join(parts[2:])
+            meeting_id = meeting_id.replace('-', '').replace(' ', '')
+            teacher_last_name = teacher.split(' ')[1]
+            creds.append([teacher, meeting_id, password])
+
+        with open('details.csv', mode='w') as details_file:
+            details_writer = csv.writer(details_file)
+            for cred in creds:
+                details_writer.writerow(cred)
+
+    creds = sorted(creds, key=lambda teacher: teacher[0].split(' ')[1])
+
+    teacher_details = []
+
+    for cred in creds:
+        teacher_details.append({'teacher': cred[0], 'id': cred[1], 'pword': cred[2]})
+
+    return teacher_details
+
+
+teacher_details = load_details()
+
+while True:
+    for count, teacher in enumerate(teacher_details):
+        print(f"{count}: {teacher['teacher']}")
+    print("x: to quit")
+    print('')
+    print('Enter the number of the teacher:')
+    teacher = input().lower()
+    if teacher == 'x':
+        break
+    if type(teacher) is not int:
+        print('must be a number')
+        print('')
+        print('')
+        continue
+    my_teacher = teacher_details[int(teacher)]
+    join_zoom(my_teacher['id'], my_teacher['pword'])
