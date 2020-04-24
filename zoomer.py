7d434cb414471f7039e3dc907295ec7964ff6a06
andy <andytwoods@gmail.com>
2020-04-24 14:14:55 +0100

    removed PyaAutoGUI craziness. Using Tom's url scheme idea.

--- zoomer.py
+++ zoomer.py
@@ -1,48 +1,22 @@
 import csv
-
-import pyautogui
+import os
 import time
 
 # majority of code stolen from
 # https://github.com/BigchillRK/Zoom-Meeting-and-Recording/blob/master/zoom_record_template1.py
+import pyperclip
 from pymsgbox import prompt
 
 
 def join_zoom(meet_id: str, password: str = None):
-    print('')
-    print('launching zoom')
-    pyautogui.press('esc', interval=0.1)
-
-    time.sleep(0.2)
-
-    # these lines are simulating starting up zoom by pressing windows key and typing zoom to open program
-    pyautogui.press('win', interval=0.1)
-    pyautogui.write('zoom')
-    pyautogui.press('enter', interval=.1)
-    # wait for the window
-
-    # time delay to factor for zoom app to load up, good buffer is like 10 sec but its case specific
-    time.sleep(.5)
-
-    # this part simulates clicking join meeting, entering meeting id and pressing enter to join
-    ##Make sure the joinButton.png file is located in the same folder as the python file or else it will not work
-    ##this tells the script where to click to join the meeting
-
-    x, y = pyautogui.locateCenterOnScreen('joinButton.png')
-    print('finding then clicking join button')
-    pyautogui.click(x, y)
-    pyautogui.press('enter', interval=.1)
-    ## the interval of 1 second is important, if not there, then the meeting id will not be inputted
-    pyautogui.write(meet_id)
-    pyautogui.press('enter', interval=.1)
+    zoom = f"zoommtg://zoom.us/join?confno={meet_id}"
 
     if password:
-        print('entering password')
-        time.sleep(.1)
-        pyautogui.press('enter', interval=.5)
-        pyautogui.write(password)
-        pyautogui.press('enter', interval=.1)
+        pyperclip.copy(password)
         print('')
+        print('copied password to clipboard')
+
+    os.system(f'start {zoom}')
 
 
 def load_details():
